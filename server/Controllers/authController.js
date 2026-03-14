53c19fef3eb024ab7c3fa0eadeeaad0859b02c50
Abhinand <abhinandatr@gmail.com>
2026-03-15 00:24:16 +0530

    backend initial commit

---
+++ server/Controllers/authController.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const generateToken =(id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:"1h"});
+};
+
+exports.registerUser = async(req,res) =>{
+
+};
+
+exports.loginUser = async(req,res) =>{
+
+};
+
+exports.getUserInfo = async(req,res) =>{
+
+};
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {registerUser,loginUser,getUserInfo} from "../Controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register",registerUser);
+
+router.post("/login",loginUser);
+
+router.get("/getUser",protect,getUserInfo);
+
+export default router;
